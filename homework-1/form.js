6edae1ffb0f49eb645446b3619db169e306c6d9f
Tyler Amos <tyler@automatedinsights.com>
2017-05-01 22:02:59 -0400

    Add form validation

--- homework-1/form.js
+++ homework-1/form.js
@@ -1,3 +1,55 @@
 $(function() {
 
+  var $form = $('form');
+
+  var validateMathInput = function(errors) {
+    var value = $('input#input-math').val();
+    if (value === '') {
+      errors.push('An answer is required for "2 + 3 = ?".');
+    } else if (value != '5') {
+      errors.push('Math is hard, but 2 + 3 does not equal ' + value + '.');
+    }
+  };
+
+  var validateAmphibianInput = function(errors) {
+    var value = $('select#select-amphibian').val();
+    if (value === '') {
+      errors.push('An answer is required for "Which one is an amphibian?".');
+    } else if (value !== 'frogs') {
+      errors.push(value + ' is not an amphibian.');
+    }
+  };
+
+  var validateColorInput = function(errors) {
+    var value = $('input[name="input-color"]:checked').val();
+    if (value === undefined) {
+      errors.push('An answer is required for "Favorite Color?".');
+    }
+  };
+
+  var validateCheckMeInput = function(errors) {
+    if (!$('input#check-me').is(':checked')) {
+      errors.push('Please select the "Check Me" checkbox.');
+    }
+  };
+
+  var validateForm = function() {
+    var errors = [];
+    validateMathInput(errors);
+    validateAmphibianInput(errors);
+    validateColorInput(errors);
+    validateCheckMeInput(errors);
+
+    if (errors.length == 0) {
+      console.log('VALID');
+    } else {
+      alert(errors.join('\n'));
+    }
+  };
+
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    validateForm();
+  });
+
 });
